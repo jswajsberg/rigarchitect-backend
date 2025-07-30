7a0c206acb1bdb875b41e3f576327e33d7b4fc31
Joseph Wajsberg <josephwajs@proton.me>
2025-07-30 17:29:20 -0400

    Initial commit

---
+++ src/main/java/com/rigarchitect/rigarchitect/RigArchitectApplication.java
@@ -0,0 +1,13 @@
+package com.rigarchitect.rigarchitect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigArchitectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RigArchitectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rigarchitect/rigarchitect/RigArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigarchitect.rigarchitect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigArchitectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
